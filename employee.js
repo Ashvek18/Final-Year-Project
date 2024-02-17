9815d7510bcb16a3cf10bb9cc140083507588857
Ashvek Padwal <89645292+Ashvek18@users.noreply.github.com>
2024-02-17 19:47:50 +0530

    employee.js

--- employee.js
+++ employee.js
@@ -1,3 +1,18 @@
+function createAlert(message, type) {
+    const alertDiv = document.createElement('div');
+    alertDiv.classList.add('alert', `alert-${type}`, 'alert-dismissible', 'fade', 'show');
+    alertDiv.setAttribute('role', 'alert');
+    alertDiv.innerHTML = `
+        ${message}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    `;
+    const timeoutDuration = 3000;
+    setTimeout(() => {
+        alertDiv.remove();
+    }, timeoutDuration);
+    return alertDiv;
+}
+
 //function to parse csv and xlsx files
 function parseFile() {
     const fileInput = document.getElementById('csvFileInput');
@@ -52,12 +67,12 @@ function parseFile() {
                     console.log('File is missing the following required columns: ' + missingColumns.join(', '));
                 }
             } else {
-                console.error('Unsupported file format.');
+                document.getElementById("alertContainer").appendChild(createAlert("Unsupported File Format", "Danger"));
             }
         };
         reader.readAsBinaryString(file);
     } else {
-        console.error('No file selected.');
+        document.getElementById("alertContainer").appendChild(createAlert("Please Select a File", "danger"));
     }
 }
 
@@ -66,7 +81,7 @@ function makePostRequest(data) {
     fetch('https://safe-gaurd-backend.vercel.app/api/employee/', {
         method: 'POST',
         headers: {
-            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzA3OTU4NzUyLCJpYXQiOjE3MDc5Mjg3NTIsImp0aSI6IjE4YWQwZTYzN2NkMjRiMDFiZDFjMDdjY2NhMWY3NjkyIiwidXNlcl9pZCI6MX0.hQBydxZlDqGoTOD3_aGv3f1OcyRokFwtQfyceqCjgbY',
+          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzA4MjAyMjg2LCJpYXQiOjE3MDgxNzIyODYsImp0aSI6IjY0MGU2YTU1MjI3MTRiOGE4OTc5NmU1M2U2MmM3Y2QzIiwidXNlcl9pZCI6MX0.AxCW6-NZxv3tNKztAA44UozmNRZ1TkumCYklmeT6SJo',
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
@@ -78,15 +93,16 @@ function makePostRequest(data) {
         return response.json();
     })
     .then(data => {
-        console.log('Data posted successfully:', data);
+        document.getElementById("alertContainer").appendChild(createAlert("Employee Added Successfully", "success"));
+        console.log(data);
         const fileInput = document.getElementById('csvFileInput');
         fileInput.value = '';
         $('#exampleModalToggle').modal('hide');
+        fetchTable();
     })
     .catch(error => {
         console.error('Error posting data:', error);
     });
-    fetchTable();
 }
 
 //add employee through form
@@ -99,11 +115,10 @@ document.querySelector('#employee-form').addEventListener('click', function (eve
         last_name: document.getElementById('exampleInputlname').value,
         phone: document.getElementById('exampleInputphone').value
     };
-
     fetch('https://safe-gaurd-backend.vercel.app/api/employee/', {
         method: 'POST',
         headers: {
-            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzA3OTU4NzUyLCJpYXQiOjE3MDc5Mjg3NTIsImp0aSI6IjE4YWQwZTYzN2NkMjRiMDFiZDFjMDdjY2NhMWY3NjkyIiwidXNlcl9pZCI6MX0.hQBydxZlDqGoTOD3_aGv3f1OcyRokFwtQfyceqCjgbY',
+          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzA4MjAyMjg2LCJpYXQiOjE3MDgxNzIyODYsImp0aSI6IjY0MGU2YTU1MjI3MTRiOGE4OTc5NmU1M2U2MmM3Y2QzIiwidXNlcl9pZCI6MX0.AxCW6-NZxv3tNKztAA44UozmNRZ1TkumCYklmeT6SJo',
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
@@ -115,17 +130,18 @@ document.querySelector('#employee-form').addEventListener('click', function (eve
         return response.json();
     })
     .then(data => {
-        console.log('Data posted successfully:', data);
+        document.getElementById("alertContainer").appendChild(createAlert("Employee Added Successfully", "success"));
+        console.log(data);
         document.getElementById('exampleInputempid').value = '';
         document.getElementById('exampleInputfname').value = '';
         document.getElementById('exampleInputlname').value = '';
         document.getElementById('exampleInputphone').value = '';
         $('#exampleModalToggle2').modal('hide');
+        fetchTable();
     })
     .catch(error => {
         console.error('Error posting data:', error);
     });
-    fetchTable();
 });
 
 
@@ -133,7 +149,7 @@ function fetchTable() {
     fetch('https://safe-gaurd-backend.vercel.app/api/employee/', {
         method: 'GET',
         headers: {
-            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzA3OTU4NzUyLCJpYXQiOjE3MDc5Mjg3NTIsImp0aSI6IjE4YWQwZTYzN2NkMjRiMDFiZDFjMDdjY2NhMWY3NjkyIiwidXNlcl9pZCI6MX0.hQBydxZlDqGoTOD3_aGv3f1OcyRokFwtQfyceqCjgbY',
+          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzA4MjAyMjg2LCJpYXQiOjE3MDgxNzIyODYsImp0aSI6IjY0MGU2YTU1MjI3MTRiOGE4OTc5NmU1M2U2MmM3Y2QzIiwidXNlcl9pZCI6MX0.AxCW6-NZxv3tNKztAA44UozmNRZ1TkumCYklmeT6SJo',
             'Content-Type': 'application/json'
         }
     })
@@ -154,8 +170,8 @@ function fetchTable() {
                     <td>${item.first_name}</td>
                     <td>${item.last_name}</td>
                     <td>${item.phone}</td>
-                    <td><i type="button" class="fa fa-user-edit" id="employeeEdit"></i></td>
-                    <td><i type="button" class="fa fa-trash text-danger" onclick=deleteEmployee(${item.emp_id}); id="employeeDelete"></i></td>
+                    <td><i type="button" class="fa fa-user-edit" id="employeeEdit" onclick="editEmployee(${item.emp_id})" data-bs-toggle="modal" data-bs-target="#exampleModalEdit"></i></td>
+                    <td><i type="button" class="fa fa-trash text-danger" onclick="deleteEmployee(${item.emp_id})"; id="employeeDelete"></i></td>
                 `;
                 tbody.appendChild(row);
             });
@@ -171,23 +187,69 @@ function fetchTable() {
 }
 fetchTable();
 
+//Function to Delete Employee
 function deleteEmployee(employeeId) {
     fetch(`https://safe-gaurd-backend.vercel.app/api/employee/${employeeId}/`, {
         method: 'DELETE',
         headers: {
-            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzA3OTU4NzUyLCJpYXQiOjE3MDc5Mjg3NTIsImp0aSI6IjE4YWQwZTYzN2NkMjRiMDFiZDFjMDdjY2NhMWY3NjkyIiwidXNlcl9pZCI6MX0.hQBydxZlDqGoTOD3_aGv3f1OcyRokFwtQfyceqCjgbY'
-            ,'Content-Type': 'application/json'
+            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzA4MjAyMjg2LCJpYXQiOjE3MDgxNzIyODYsImp0aSI6IjY0MGU2YTU1MjI3MTRiOGE4OTc5NmU1M2U2MmM3Y2QzIiwidXNlcl9pZCI6MX0.AxCW6-NZxv3tNKztAA44UozmNRZ1TkumCYklmeT6SJo',
+            'Content-Type': 'application/json'
         }
     })
     .then(response => {
         if (!response.ok) {
             throw new Error('Failed to delete employee');
         }
+        document.getElementById("alertContainer").appendChild(createAlert("Employee Deleted Successfully", "success"));
         fetchTable();
     })
     .catch(error => {
         console.error('Error deleting employee:', error);
     });
 }
-
 fetchTable();
+
+// function to edit employee
+function editEmployee(emp_id) {
+    document.getElementById('editempid').value = emp_id;
+    document.getElementById('edit-form').addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent the form from submitting normally
+
+        const data = {
+            emp_id:emp_id.toString(),
+            first_name: document.getElementById('editfname').value,
+            last_name: document.getElementById('editlname').value,
+            phone: document.getElementById('editphone').value
+        };
+console.log(data);
+        // Send PATCH request to update employee details
+        fetch(`https://safe-gaurd-backend.vercel.app/api/employee/${emp_id}/`, {
+            method: 'PATCH',
+            headers: {
+                'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzA4MjAyMjg2LCJpYXQiOjE3MDgxNzIyODYsImp0aSI6IjY0MGU2YTU1MjI3MTRiOGE4OTc5NmU1M2U2MmM3Y2QzIiwidXNlcl9pZCI6MX0.AxCW6-NZxv3tNKztAA44UozmNRZ1TkumCYklmeT6SJo',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            document.getElementById("alertContainer").appendChild(createAlert("Employee Updated Successfully", "success"));
+            console.log(data);
+            // Clear input fields and close modal
+            document.getElementById('editfname').value = '';
+            document.getElementById('editlname').value = '';
+            document.getElementById('editphone').value = '';
+            fetchTable();
+        })
+        .catch(error => {
+            console.error('Error updating employee details:', error);
+        });
+    });
+}
+
+
